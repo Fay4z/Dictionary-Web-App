c66f93323347add6ef3edd6972e973a91ae27886
Fay4z <fayazjr212@gmail.com>
2023-12-04 07:41:31 +0530

    refactor: all components rendered from app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,57 @@
+import { useState } from "react";
 import FormComponent from "./components/FormComponent"
 import DisplayData from "./components/DisplayData"
  
 function App() {
+    const [inputWordData, setInputWordData] = useState("");
+    const [synonyms, setSynonyms] = useState();
+    const [antonyms, setAntonyms] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
+
+
+    const getSynonyms = async(inputWord) => {
+        const response = await fetch(`https://api.datamuse.com/words?rel_syn=${inputWord}`)
+        const data = await response.json();
+        setSynonyms(data);
+    }
+
+    const getAntonyms = async(inputWord) => {
+        const response = await fetch(`https://api.datamuse.com/words?rel_ant=${inputWord}`)
+        const data = await response.json();
+        setAntonyms(data);
+    }
+
+    const handleFormSubmit = async (inputWord) => {
+        // e.preventDefault();
+        console.log("hi");
+        setIsLoading(true);
+        console.log(inputWord);
+        setInputWordData(inputWord);
+        try{
+            await getSynonyms(inputWord);
+            await getAntonyms(inputWord);
+        }catch(e){
+            setError(e);
+            console.log(e);
+        }
+        console.log("bye");
+        setIsLoading(false);
+    }
 
   return (
     <div className="max-w-7xl m-auto">
       <h1>WELCOME TO SIMPLE DICTIONARY</h1>
       <p>Search a word to get its antonyms and synonyms</p>
 
-      <FormComponent />
+      <FormComponent onFormSubmit={handleFormSubmit}/>
+      {error && <p>Error {error}</p>}
+      {isLoading ? 
+        <p>Loading...</p>
+        : (
+          <DisplayData synonyms={synonyms} antonyms={antonyms} inputWordData={inputWordData}/>
+        )
+      }
     </div>
   )
 }
--- client/src/components/FormComponent.jsx
+++ client/src/components/FormComponent.jsx
@@ -1,45 +1,18 @@
 import { useState } from "react";
-import DisplayData from "./DisplayData";
 
-function FormComponent() {
+function FormComponent({onFormSubmit}) {
 
     const [inputWord, setInputWord] = useState("");
-    const [synonyms, setSynonyms] = useState();
-    const [antonyms, setAntonyms] = useState();
-    const [isLoading, setIsLoading] = useState(false);
-    const [serror, setError] = useState("");
-
-
-    const getSynonyms = async() => {
-        const response = await fetch(`https://api.datamuse.com/words?rel_syn=${inputWord}`)
-        const data = await response.json();
-        setSynonyms(data);
-    }
-
-    const getAntonyms = async() => {
-        const response = await fetch(`https://api.datamuse.com/words?rel_ant=${inputWord}`)
-        const data = await response.json();
-        setAntonyms(data);
-    }
-
-    const handleFormSubmit = async (e) => {
+    
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("hi");
-        setIsLoading(true);
-        try{
-            await getSynonyms();
-            await getAntonyms();
-        }catch(e){
-            setError(e);
-        }
-        console.log("bye");
-        setIsLoading(false);
+        onFormSubmit(inputWord);
     }
 
     return ( 
         <div>
             <h1>This is Form Component</h1>
-            <form onSubmit={handleFormSubmit}>
+            <form onSubmit={handleSubmit}>
                 <input 
                     type="text" 
                     className="border p-3 w-96 rounded"
@@ -48,11 +21,6 @@ function FormComponent() {
                 />
                 <button className="border bg-green-500 p-3 px-5 rounded ml-6">Search</button>
             </form>
-            {isLoading ? 
-                <p>Loading ....</p>
-                :
-                <DisplayData inputWord={inputWord} synonyms={synonyms} antonyms={antonyms} /> 
-            }
         </div>
      );
 }
